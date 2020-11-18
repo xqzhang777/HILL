f5efeeb8e112be27760ca49f265f1238b1b58c4d
Wen Jiang <wjiang@gmail.com>
2020-11-17 20:21:10 -0500

    add simulated helix image/power spectra

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ def main():
 
     with col1:
         with st.beta_expander(label="README", expanded=False):
-            st.write("This Web app considers a biological helical structure as the product of a continous helix and a set of parallel planes, and based on the covolution theory, the Fourier Transform (FT) of a helical structure would be the convolution of the FT of the continous helix and the FT of the planes.  \nThe FT of a continous helix consists of equally spaced layer planes (3D) or layer lines (2D projection) that can be described by Bessel functions of increasing orders (0, +/-1, +/-2, ...) from the Fourier origin (i.e. equator). The spacing between the layer planes/lines is determined by the helical pitch (i.e. the shift along the helical axis for a 360 degree turn of the helix). If the structure has additional cyclic symmetry (for example, C6) around the helical axis, only the layer plane/line orders of integer multiplier of the symmetry (e.g. 0, +/-6, +/-12, ...) are visible. The overall shape of the FT is similar to a X symbol.  \nThe FT of the parallel planes consists of equally spaced points along the helical axis (i.e. meridian) with the spacing being determined by the helical rise.  \nThe convolution of these two components (X-shaped layer lines and points along the meridian) generates the layer line patterns seen in the power spectra of the projection images of helical structures. The helical indexing task is thus to identify the helical rise, pitch (or twist), and cyclic symmetry that would predict a lay line pattern to explain the observed the layer lines in the power spectra. This Web app allows you to interactively change the helical parameters and superimpose the predicted layer liines on the power spectra to complete the helical indexing task.  \n  \nPS: power spectra; YP: Y-axis power spectra profile; X: the X pattern; LL: layer lines; m: indices of the X-patterns along the meridian")
+            st.write("This Web app considers a biological helical structure as the product of a continous helix and a set of parallel planes, and based on the covolution theory, the Fourier Transform (FT) of a helical structure would be the convolution of the FT of the continous helix and the FT of the planes.  \nThe FT of a continous helix consists of equally spaced layer planes (3D) or layer lines (2D projection) that can be described by Bessel functions of increasing orders (0, +/-1, +/-2, ...) from the Fourier origin (i.e. equator). The spacing between the layer planes/lines is determined by the helical pitch (i.e. the shift along the helical axis for a 360 ° turn of the helix). If the structure has additional cyclic symmetry (for example, C6) around the helical axis, only the layer plane/line orders of integer multiplier of the symmetry (e.g. 0, +/-6, +/-12, ...) are visible. The overall shape of the FT is similar to a X symbol.  \nThe FT of the parallel planes consists of equally spaced points along the helical axis (i.e. meridian) with the spacing being determined by the helical rise.  \nThe convolution of these two components (X-shaped layer lines and points along the meridian) generates the layer line patterns seen in the power spectra of the projection images of helical structures. The helical indexing task is thus to identify the helical rise, pitch (or twist), and cyclic symmetry that would predict a lay line pattern to explain the observed the layer lines in the power spectra. This Web app allows you to interactively change the helical parameters and superimpose the predicted layer liines on the power spectra to complete the helical indexing task.  \n  \nPS: power spectra; YP: Y-axis power spectra profile; X: the X pattern; LL: layer lines; m: indices of the X-patterns along the meridian")
         
         label = "Input a url of 2D projection image(s) or an EMDB 3D map:"
         image_url = st.text_input(label=label, value=data_example.url)
@@ -25,24 +25,43 @@ def main():
             image_index = 0
         data = data_all[image_index]
         ny, nx = data.shape
-        st.image(data, width=nx, caption=f"Orignal image ({nx}x{ny})", clamp=[data.min(), data.max()])
-        angle = st.number_input('Rotate (Degree)', value=0.0, min_value=-180., max_value=180., step=1.0)
+        original_image = st.empty()
+        with original_image:
+            st.image(data, width=nx, caption=f"Orignal image ({nx}x{ny})", clamp=True)
+        angle = st.number_input('Rotate (°)', value=0.0, min_value=-180., max_value=180., step=1.0)
+        rotated_image = st.empty()
         if angle!=0:
             data = rotate_image(data, angle)
-            st.image(data, width=nx, caption="Rotated image", clamp=[data.min(), data.max()])
-        ploty = st.empty()
+            with rotated_image:
+                st.image(data, width=nx, caption="Rotated image", clamp=True)
         plotx = st.empty()
+        ploty = st.empty()
         st.markdown("*Developed by the [Jiang Lab@Purdue University](https://jiang.bio.purdue.edu). Report problems to Wen Jiang (jiang12 at purdue.edu)*")
 
     with col2:
-        apix = st.number_input('Pixel size (Angstrom/pixel)', value=apix, min_value=0.1, max_value=10., step=0.01, format="%.4f")
-        twist = st.number_input('Twist (degree)', value=data_example.twist, min_value=-180.0, max_value=180.0, step=1.0, format="%.2f")
-        rise = st.number_input('Rise (Angstrom)', value=data_example.rise, min_value=-180.0, max_value=180.0, step=1.0, format="%.2f")
+        apix = st.number_input('Pixel size (Å/pixel)', value=apix, min_value=0.1, max_value=10., step=0.01, format="%.4f")
+        twist = st.number_input('Twist (°)', value=data_example.twist, min_value=-180.0, max_value=180.0, step=1.0, format="%.2f")
+        rise = st.number_input('Rise (Å)', value=data_example.rise, min_value=-180.0, max_value=180.0, step=1.0, format="%.2f")
         csym = st.number_input('Csym', value=data_example.csym, min_value=1, max_value=16, step=1)
-        radius = st.number_input('Radius (Angstrom)', value=data_example.diameter/2., min_value=10.0, max_value=1000.0, step=10., format="%.1f")
+        radius = st.number_input('Radius (Å)', value=data_example.diameter/2., min_value=10.0, max_value=1000.0, step=10., format="%.1f")
         pnx = st.number_input('X-dim padding (pixels)', value=max(nx, 512), min_value=nx, step=2)
         pny = st.number_input('Y-dim padding (pixels)', value=max(ny, 1024), min_value=ny, step=2)
-        cutoff_res = st.number_input('Limit FFT to resolution (Angstrom)', value=max(rise*0.4, 2*apix), min_value=2*apix, step=1.0)
+        cutoff_res = st.number_input('Limit FFT to resolution (Å)', value=max(float(round(rise*0.3)), 2*apix), min_value=2*apix, step=1.0)
+        with st.beta_expander(label="Simulate the helix with Gaussians", expanded=False):
+            ball_radius = st.number_input('Gaussian radius (Å)', value=0.0, min_value=0.0, max_value=radius, step=5.0, format="%.1f")
+            az = st.number_input('Azimuthal angle (°)', value=0, min_value=0, max_value=360, step=1, format="%.1f")
+
+    if ball_radius>0:
+        proj = simulate_helix(twist, rise, csym, helical_radius=radius, ball_radius=ball_radius*0.8, ny=data.shape[0], nx=data.shape[1], apix=apix, angle0=az)
+        proj = tapering(proj, fraction=0.3)
+        if angle:
+            image_container = rotated_image
+            image_label = "Rotated image"
+        else:
+            image_container = original_image
+            image_label = "Original image"
+        with image_container:
+            st.image([data, proj], width=data.shape[1], caption=[image_label, "Simulated"], clamp=True)
 
     with ploty:
         y = np.arange(-ny//2, ny//2)*apix
@@ -51,7 +70,7 @@ def main():
 
         from bokeh.plotting import figure
         tools = 'box_zoom,crosshair,hover,pan,reset,save,wheel_zoom'
-        p = figure(x_axis_label="pixel value", y_axis_label="y (Angstrom)", frame_height=ny, tools=tools)
+        p = figure(x_axis_label="pixel value", y_axis_label="y (Å)", frame_height=ny, tools=tools)
         p.line(xmax, y, line_width=2, color='red')
         p.line(xmean, y, line_width=2, color='blue')
         st.bokeh_chart(p, use_container_width=True)
@@ -63,14 +82,12 @@ def main():
 
         from bokeh.plotting import figure
         tools = 'box_zoom,crosshair,hover,pan,reset,save,wheel_zoom'
-        p = figure(x_axis_label="x (Angstrom)", y_axis_label="pixel value", frame_height=ny, tools=tools)
+        p = figure(x_axis_label="x (Å)", y_axis_label="pixel value", frame_height=ny, tools=tools)
         p.line(x, ymax, line_width=2, color='red', legend_label="max")
         p.line(x, ymean, line_width=2, color='blue', legend_label="mean")
         p.legend.location = "top_right"
         st.bokeh_chart(p, use_container_width=True)
 
-    data_padded = pad_2d_image(data, pnx, pny)
-    pwr = compute_power_spectra(data_padded, apix=apix, cutoff_res=cutoff_res, high_pass_fraction=0.004)
     m_groups = compute_layer_line_positions(twist=twist, rise=rise, csym=csym, radius=radius, cutoff_res=cutoff_res)
     ng = len(m_groups)
 
@@ -89,39 +106,92 @@ def main():
             show_choices[lg] = st.checkbox(label=str(lg), value=value)
     
     with col4:
+        if not show_pwr:
+            return
+
+        data = tapering(data, fraction=0.1)
+        data_padded = pad_2d_image(data, pnx, pny)
+        pwr = compute_power_spectra(data_padded, apix=apix, cutoff_res=cutoff_res, high_pass_fraction=0.004)
+
         ny, nx = pwr.shape
         dsy = 1/(ny//2*cutoff_res)
         dsx = 1/(nx//2*cutoff_res)
 
-        if show_pwr:
-            from bokeh.models import LinearColorMapper
-            tools = 'box_zoom,crosshair,pan,reset,save,wheel_zoom'
-            fig = figure(title_location="below", frame_width=nx, frame_height=ny, 
+        from bokeh.models import LinearColorMapper
+        tools = 'box_zoom,crosshair,pan,reset,save,wheel_zoom'
+        fig = figure(title_location="below", frame_width=nx, frame_height=ny, 
+            x_axis_label=None, y_axis_label=None, 
+            x_range=(-nx//2*dsx, nx//2*dsx), y_range=(-ny//2*dsy, ny//2*dsy), 
+            tools=tools)
+        fig.grid.visible = False
+        fig.title.text = f"Power Spectra"
+        fig.title.align = "center"
+        fig.title.text_font_size = "20px"
+
+        sy, sx = np.meshgrid(np.arange(-ny//2, ny//2)*dsy, np.arange(-nx//2, nx//2)*dsx, indexing='ij', copy=False)
+        resx = np.abs(1./sx)
+        resy = np.abs(1./sy)
+        res  = 1./np.hypot(sx, sy)
+
+        source_data = dict(image=[pwr], x=[-nx//2*dsx], y=[-ny//2*dsy], dw=[nx*dsx], dh=[ny*dsy], resx=[resx], resy=[resy], res=[res])
+        from bokeh.models import LinearColorMapper
+        palette = 'Viridis256' if show_pseudocolor else 'Greys256'
+        color_mapper = LinearColorMapper(palette=palette)    # Greys256, Viridis256
+        image = fig.image(source=source_data, image='image', color_mapper=color_mapper,
+                    x='x', y='y', dw='dw', dh='dh'
+                )
+        # add hover tool only for the image
+        from bokeh.models.tools import HoverTool
+        tooltips = [("Res", "@resÅ"), ('Res x', '@resxÅ'), ('Res y', '@resyÅ'), ('PS', '@image')]
+        image_hover = HoverTool(renderers=[image], tooltips=tooltips)
+        fig.add_tools(image_hover)
+
+        if ball_radius>0:
+            proj_padded = pad_2d_image(proj, pnx, pny)
+            proj_pwr = compute_power_spectra(proj_padded, apix=apix, cutoff_res=cutoff_res, high_pass_fraction=0.004)
+
+            fig_proj = figure(title_location="below", frame_width=nx, frame_height=ny, 
                 x_axis_label=None, y_axis_label=None, 
                 x_range=(-nx//2*dsx, nx//2*dsx), y_range=(-ny//2*dsy, ny//2*dsy), 
                 tools=tools)
-            fig.grid.visible = False
-            fig.title.text = f"Power Spectra"
-            fig.title.align = "center"
-            fig.title.text_font_size = "20px"
+            fig_proj.grid.visible = False
+            fig_proj.title.text = f"Simulated Power Spectra"
+            fig_proj.title.align = "center"
+            fig_proj.title.text_font_size = "20px"
 
-            sy, sx = np.meshgrid(np.arange(-ny//2, ny//2)*dsy, np.arange(-nx//2, nx//2)*dsx, indexing='ij', copy=False)
-            resx = np.abs(1./sx)
-            resy = np.abs(1./sy)
-            res  = 1./np.hypot(sx, sy)
-
-            source_data = dict(image=[pwr], x=[-nx//2*dsx], y=[-ny//2*dsy], dw=[nx*dsx], dh=[ny*dsy], resx=[resx], resy=[resy], res=[res])
-            from bokeh.models import LinearColorMapper
-            palette = 'Viridis256' if show_pseudocolor else 'Greys256'
-            color_mapper = LinearColorMapper(palette=palette)    # Greys256, Viridis256
-            image = fig.image(source=source_data, image='image', color_mapper=color_mapper,
+            source_data["image"] = [proj_pwr]
+            proj_image = fig_proj.image(source=source_data, image='image', color_mapper=color_mapper,
                         x='x', y='y', dw='dw', dh='dh'
                     )
             # add hover tool only for the image
-            from bokeh.models.tools import HoverTool
-            tooltips = [("Res", "@resÅ"), ('Res x', '@resxÅ'), ('Res y', '@resyÅ'), ('PS', '@image')]
-            image_hover = HoverTool(renderers=[image], tooltips=tooltips)
-            fig.add_tools(image_hover)
+            image_hover = HoverTool(renderers=[proj_image], tooltips=tooltips)
+            fig_proj.add_tools(image_hover)
+
+            # link the crosshair tool
+            from bokeh.models import CrosshairTool
+            crosshair = CrosshairTool(dimensions="both")
+            fig.add_tools(crosshair)
+            fig_proj.add_tools(crosshair)
+        else:
+            fig_proj = None
+
+        if show_yprofile:
+            y=np.arange(-ny//2, ny//2)*dsy
+            ll_profile = np.mean(pwr, axis=1)             # entire layerline
+
+            tools = 'box_zoom,crosshair,hover,pan,reset,save,wheel_zoom'
+            tooltips = [('Res y', '@resyÅ'), ('PS', '$x')]
+            fig_y = figure(frame_width=nx//2, frame_height=ny, y_range=fig.y_range, title=None, tools=tools, tooltips=tooltips)
+            source_data = dict(ll=ll_profile, y=y, resy=np.abs(1./y))
+            fig_y.line(source=source_data, x='ll', y='y', line_width=2, color='blue')
+
+            # link the crosshair tool
+            from bokeh.models import CrosshairTool
+            crosshair = CrosshairTool(dimensions="both")
+            fig.add_tools(crosshair)
+            fig_y.add_tools(crosshair)
+        else:
+            fig_y = None
 
         from bokeh.palettes import viridis, gray
         if show_pseudocolor:
@@ -140,6 +210,8 @@ def main():
                 x, y = m_groups[m]["LL"]
                 color = ll_colors[mi]
                 fig.ellipse(x, y, width=width, height=height, line_width=4, line_color=color, fill_alpha=0)
+                if fig_proj:
+                    fig_proj.ellipse(x, y, width=width, height=height, line_width=4, line_color=color, fill_alpha=0)
 
         if show_X:
             for mi, m in enumerate(m_groups.keys()):
@@ -147,34 +219,55 @@ def main():
                 x, y = m_groups[m]["X"]
                 color = ll_colors[mi]
                 fig.multi_line(x, y, line_width=4, line_color=color, line_dash="dashed")
+                if fig_proj:
+                    fig_proj.multi_line(x, y, line_width=4, line_color=color, line_dash="dashed")
 
-        if show_yprofile:
-            y=np.arange(-ny//2, ny//2)*dsy
-            ll_profile = np.mean(pwr, axis=1)             # entire layerline
-
+        if fig_proj or fig_y:
+            figs = [f for f in [fig, fig_y, fig_proj] if f]
             from bokeh.layouts import gridplot
-            tools = 'box_zoom,crosshair,hover,pan,reset,save,wheel_zoom'
-            tooltips = [('Res y', '@resyÅ'), ('PS', '$x')]
-            fig_y = figure(frame_width=nx//2, frame_height=ny, y_range=fig.y_range, title=None, tools=tools, tooltips=tooltips)
-            source_data = dict(ll=ll_profile, y=y, resy=np.abs(1./y))
-            fig_y.line(source=source_data, x='ll', y='y', line_width=2, color='blue')
-
-            # link the crosshair tool
-            from bokeh.models import CrosshairTool
-            crosshair = CrosshairTool(dimensions="both")
-            fig.add_tools(crosshair)
-            fig_y.add_tools(crosshair)
-
-            fig = gridplot([[fig, fig_y]], toolbar_location='right')
+            fig = gridplot([figs], toolbar_location='right')
         
         st.bokeh_chart(fig, use_container_width=False)
 
         return
 
 @st.cache(persist=True, show_spinner=False)
+def simulate_helix(twist, rise, csym, helical_radius, ball_radius, ny, nx, apix, angle0=None):
+    def simulate_projection(centers, sigma, ny, nx, apix):
+        sigma2 = sigma*sigma
+        d = np.zeros((ny, nx))
+        Y, X = np.meshgrid(np.arange(0, ny, dtype=np.float)-ny//2, np.arange(0, nx, dtype=np.float)-nx//2, indexing='ij')
+        X *= apix
+        Y *= apix
+        for ci in range(len(centers)):
+            yc, xc = centers[ci]
+            x = X-xc
+            y = Y-yc
+            d += np.exp(-(x*x+y*y)/sigma2)
+        return d
+    def helical_unit_positions(twist, rise, csym, radius, height, angle0=0):
+        imax = int(height/rise)
+        i0 = -imax
+        i1 = imax
+        
+        centers = np.zeros(((2*imax+1)*csym, 2), dtype=np.float)
+        for i in range(i0, i1+1):
+            y = rise*i
+            for si in range(csym):
+                angle = np.deg2rad(twist*i + si*360./csym + angle0)
+                x = np.sin(angle) * radius
+                centers[i*csym+si, 0] = y
+                centers[i*csym+si, 1] = x
+        return centers
+    if angle0 is None: angle0 = np.random.uniform(0, 360)
+    centers = helical_unit_positions(twist, rise, csym, helical_radius, height=ny*apix, angle0=angle0)
+    projection = simulate_projection(centers, ball_radius, ny, nx, apix)
+    return projection
+
+@st.cache(persist=True, show_spinner=False)
 def compute_layer_line_positions(twist, rise, csym, radius, cutoff_res, m=[]):
     def pitch(twist, rise):
-        p = np.abs(rise * 360./twist)   # pitch in Angstrom
+        p = np.abs(rise * 360./twist)   # pitch in Å
         return p
 
     def X_line_slope(twist, rise, csym, radius):
@@ -261,6 +354,19 @@ def pad_2d_image(data, nx, ny):
     return data2
 
 @st.cache(persist=True, show_spinner=False)
+def tapering(data, fraction=0):
+    if not (0<fraction<1): return data
+    ny, nx = data.shape
+    Y, _ = np.meshgrid(np.arange(0, ny, dtype=np.float)-ny//2, np.arange(0, nx, dtype=np.float)-nx//2, indexing='ij')
+    Y = np.abs(Y / (ny//2))
+    Y = (Y-(1-fraction))/fraction
+    inner = Y<0
+    Y = (1. + np.cos(Y*np.pi))/2.0
+    Y[inner]=1
+    data2 = data * Y
+    return data2
+
+@st.cache(persist=True, show_spinner=False)
 def rotate_image(data, angle=0):
     if angle==0: return data
     from skimage.transform import rotate
@@ -329,7 +435,7 @@ class Data(object):
         self.url = url
 
 data_examples = [
-    Data(twist=29.40, rise=21.92, csym=6, diameter=135, apix=2.3438, url="https://tinyurl.com/y5tq9fqa")
+    Data(twist=29.40, rise=21.92, csym=6, diameter=138, apix=2.3438, url="https://tinyurl.com/y5tq9fqa")
 ]
 data_example = data_examples[0]
 
