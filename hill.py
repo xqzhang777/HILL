55c8baf69b171457402c15d56fe25df4f162a9d6
Wen Jiang <wjiang@gmail.com>
2021-12-11 19:21:12 -0500

    auto-convert cloud drive preview url to direct download url

--- hill.py
+++ hill.py
@@ -695,7 +695,9 @@ def obtain_input_image(column, param_i=0, image_index_sync=0):
                     image_url = st.text_input(label=label, key=f'url_{param_i}', help="An online url (http:// or ftp://) or a local file path (/path/to/your/structure.mrc)").strip()
                     is_pwr_auto = image_url.find("ps.mrcs")!=-1
                     is_pd_auto = image_url.find("pd.mrcs")!=-1
-                    data_all, apix_auto = get_2d_image_from_url(image_url)
+                    with st.spinner(f'Downloading {image_url.strip()}'):
+                        image_url = get_direct_url(image_url)    # convert cloud drive indirect url to direct url
+                        data_all, apix_auto = get_2d_image_from_url(image_url)
             nz, ny, nx = data_all.shape
             if nz==1:
                 is_3d = False
@@ -1405,7 +1407,7 @@ def get_emdb_map(emd_id: str):
 def get_2d_image_from_url(url):
     ds = np.DataSource(None)
     if not ds.exists(url):
-        st.error(f"ERROR: {url} does not exist")
+        st.error(f"ERROR: {url} could not be downloaded. If this url points to a cloud drive file, make sure the link is a direct download link instead of a link for preview")
         st.stop()
     with ds.open(url) as fp:
         data = get_2d_image_from_file(fp.name)
@@ -1494,6 +1496,28 @@ def set_session_state_from_query_params():
                 st.session_state[k] = ast.literal_eval(v)
             except:
                 st.session_state[k] = v
+
+def get_direct_url(url):
+    import re
+    if url.startswith("https://drive.google.com/file/d/"):
+        hash = url.split("/")[5]
+        return f"https://drive.google.com/uc?export=download&id={hash}"
+    elif url.startswith("https://app.box.com/s/"):
+        hash = url.split("/")[-1]
+        return f"https://app.box.com/shared/static/{hash}"
+    elif url.startswith("https://www.dropbox.com"):
+        if url.find("dl=1")!=-1: return url
+        elif url.find("dl=0")!=-1: return url.replace("dl=0", "dl=1")
+        else: return url+"?dl=1"
+    elif url.find("sharepoint.com")!=-1 and url.find("guestaccess.aspx")!=-1:
+        return url.replace("guestaccess.aspx", "download.aspx")
+    elif url.startswith("https://1drv.ms"):
+        import base64
+        data_bytes64 = base64.b64encode(bytes(url, 'utf-8'))
+        data_bytes64_String = data_bytes64.decode('utf-8').replace('/','_').replace('+','-').rstrip("=")
+        return f"https://api.onedrive.com/v1.0/shares/u!{data_bytes64_String}/root/content"
+    else:
+        return url
 
 @st.experimental_memo(persist='disk', show_spinner=False)
 def setup_anonymous_usage_tracking():
